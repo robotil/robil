acca9082e7b2c63e963f3d012d28269dc00b2b4a
Ari Yakir <ariy@laptop.(none)>
2012-11-11 13:29:02 +0200

    retrying again...

---
+++ c34_Executer/includes/BTExecuter/TaskProxyTableXML.h
@@ -0,0 +1,28 @@
+/*
+ * TestTaskProxyTable.h
+ *
+ *  Created on: Oct 2, 2012
+ *      Author: dan
+ */
+
+#ifndef TEST2TASKPROXYTABLEXML_H_
+#define TEST2TASKPROXYTABLEXML_H_
+
+#include "TaskProxyTable.h"
+
+
+class TaskProxyTableXML: public TaskProxyTable {
+public:
+	TaskProxyTableXML(std::string pathToTaskList, BTTaskProxyCreator::Ref creator);
+	TaskProxyTableXML(std::istream& pathToTaskList, BTTaskProxyCreator::Ref creator);
+	virtual ~TaskProxyTableXML();
+
+
+//	virtual void add(std::string taskname, BTTask::Ref taskaccess);
+//	virtual bool contains(std::string tn);
+//	virtual void remove(std::string tn);
+//	virtual BTTask::Ref get(std::string tn);
+
+};
+
+#endif /* TEST2TASKPROXYTABLE_H_ */
