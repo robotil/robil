c14de3162d7ae64c52bd111480ef85f83b2c0f45
Ari Yakir <ariy@laptop.(none)>
2012-11-12 13:54:27 +0200

    new modules fixed naming conventions

---
+++ C23_ObjectRecognition/src/C23_objectRecognition/msg/__init__.py
@@ -0,0 +1,7 @@
+from ._C23C0_OPO import *
+from ._C0C23_SAR import *
+from ._C0C23_SEOB import *
+from ._C0C23_SEC import *
+from ._C23C0_OD import *
+from ._C23C0_ODIM import *
+from ._TBD import *
