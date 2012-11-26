7ba75fe458dbe5773b671c18dd0b3bf6a6defa76
Your Name <you@example.com>
2012-11-26 11:51:11 +0200

    monitor update

--- c35_monitoring/scripts/monitor.py
+++ c35_monitoring/scripts/monitor.py
@@ -9,12 +9,16 @@ def callback_finger_grab(data):
 
 def callbackArmControlFeedback(data):
     rospy.loginfo(rospy.get_name()+"arm control action percent complete: %f",data.feedback.progress)
+   
+def callbackSittingControlFeedback(data)
+    rospy.loginfo(rospy.get_name()+"sitting action percent complete: %i",data.feedback.percent_done)
     
     
 def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("/finger_grab/feedback", c35_monitoring.msg.finger_grabActionFeedback, callback_finger_grab)
     rospy.Subscriber("/arm_control/feedback", c35_monitoring.msg.arm_controlActionFeedback, callbackArmControlFeedback)
+    rospy.Subscriber("/sitting/feedback",c35_monitoring.msg.sittingFeedback,callbackSittingControlFeedback)
     rospy.spin()
 
 if __name__ == '__main__':
