01ce34d360d15a4089d1ce1db891a6b7a83da77e
yakirari <yakirari@gmail.com>
2012-12-20 20:03:58 +0200

    a

--- C41_BodyControl/src/C41_BodyControl/BodyControlServer.py
+++ C41_BodyControl/src/C41_BodyControl/BodyControlServer.py
@@ -43,7 +43,7 @@ class BodyControlServer(object):
 
       print "PARAM FAUND"
 
-#      _arm = rospy.Publisher('/r_arm_ely_position_controller/command', Float64)
+      _arm = rospy.Publisher('/r_arm_ely_position_controller/command', Float64)
       #### DEFINE SLEEP DURATION BETWEEN TASK LOOP ITERATIONS ####
       r = rospy.Rate(100)
 
@@ -59,50 +59,12 @@ class BodyControlServer(object):
 		    
     	#### HERE PROCESS TASK ####
 	print "TASK PROCESS"
-#   	t = 6 * rospy.get_time()
-#    	next_pos =  0.4 + 0.4 * math.sin(t)
-#    	_arm.publish(next_pos)
+   	t = 6 * rospy.get_time()
+    	next_pos =  0.4 + 0.4 * math.sin(t)
+    	_arm.publish(next_pos)
 	     
 	r.sleep()
       
-    if goal.parameters == "param=Walk" :
-	
-      print "working on it"
-	
-      traj_yaml = yaml.load(file("/home/ariy/robil/Traj_data.yaml", 'r'))
-      traj_name = "step_and_fall"
-      if not traj_name in traj_yaml:
-	print "unable to find trajectory %s in %s" % (traj_name, sys.argv[1])
-	sys.exit(1)
-      traj_len = len(traj_yaml[traj_name])
-      traj_client = actionlib.SimpleActionClient( \
-			         '/drc_controller/follow_joint_trajectory', \
-			         FollowJointTrajectoryAction)
-      traj_client.wait_for_server()
-      # now, build the trajectory-action goal message from the YAML snippet
-      goal = FollowJointTrajectoryGoal()
-      goal.trajectory.joint_names = ['l_leg_uhz', 'l_leg_mhx', 'l_leg_lhy',
-			         'l_leg_kny', 'l_leg_uay', 'l_leg_lax',
-			         'r_leg_uhz', 'r_leg_mhx', 'r_leg_lhy',
-			         'r_leg_kny', 'r_leg_uay', 'r_leg_lax',
-			         'l_arm_usy', 'l_arm_shx', 'l_arm_ely',
-			         'l_arm_elx', 'l_arm_uwy', 'l_arm_mwx',
-			         'r_arm_usy', 'r_arm_shx', 'r_arm_ely',
-			         'r_arm_elx', 'r_arm_uwy', 'r_arm_mwx',
-			         'neck_ay', 
-			         'back_lbz', 'back_mby', 'back_ubx']
-      goal.trajectory.points = [ trajectory_msgs.msg.JointTrajectoryPoint() for x in xrange(0, traj_len) ]
-      t = 0.0
-      for i in xrange(0, traj_len):
-	  y = traj_yaml[traj_name][i]
-	  goal_pt = goal.trajectory.points[i]
-	  t += float(y[0])
-	  goal_pt.time_from_start = rospy.Duration.from_sec(t)
-	  goal_pt.velocities = [0] * 28
-	  goal_pt.positions = [ float(x) for x in y[1].split() ]
-      traj_client.send_goal(goal)
-      traj_client.wait_for_result(rospy.Duration.from_sec(t + 3))
-
     if task_success:
       self._result.success = task_result;
       rospy.loginfo("%s: Succeeded", self._action_name);
