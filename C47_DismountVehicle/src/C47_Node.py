17303d6adde84c4059a9911c71128382cad86237
sharon <sharon.handzel@gmail.com>
2013-01-03 16:36:13 +0200

    upload a python module of C47

---
+++ C47_DismountVehicle/src/C47_Node.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('C47_DismountVehicle')
+import rospy, math
+
+import actionlib
+from C46_Mount_server import DismountVehicleServer
+from std_msgs.msg import Float64
+
+if __name__ == '__main__':
+  rospy.init_node('C47_Dismount')
+  DismountVehicleServer()
+  rospy.spin()
