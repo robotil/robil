8e79c88ac6d8ad11722e7b08afe61d46f9274cb3
danerde <danerde@gmail.com>
2013-01-06 18:48:28 +0200

    path planning (not finished)

---
+++ C31_PathPlanner/src/Map.h
@@ -0,0 +1,70 @@
+/*
+ * Map.h
+ *
+ *  Created on: Jan 6, 2013
+ *      Author: dan
+ */
+
+#ifndef COGNITEAM_PATHPLANNING_MAP_H_
+#define COGNITEAM_PATHPLANNING_MAP_H_
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string.h>
+#include <sstream>
+#include <math.h>
+using namespace std;
+
+//typedef vector<vector<unsigned int> > RAWMAP;
+
+
+class Map{
+public:
+	enum STATUS{ST_AVAILABLE=0,ST_BLOCKED,ST_UNCHARTED};
+
+
+	Map(int w, int h);
+	Map(int w, int h, char* cmap);
+	Map(const Map& map);
+
+	class MapCreator{
+	public:
+		vector<char> data;
+		size_t w, h;
+		Map map()const{ Map m(w, h); m._data = data; return m; }
+	};
+
+	size_t index(int x, int y)const{ return y*w()+x; }
+	char operator()(int x, int y)const{
+		return getByIndex(index(x,y));
+	}
+	char& operator()(int x, int y){
+		return getByIndex(index(x,y));
+	}
+
+	size_t w()const{ return _w; }
+	size_t h()const{ return _h; }
+
+	char str(int x, int y)const{
+		char c = (*this)(x,y);
+		if(c==ST_AVAILABLE) return '.';
+		if(c==ST_BLOCKED) return 'B';
+		if(c==ST_UNCHARTED) return '-';
+		return '?';
+	}
+
+private:
+	char getByIndex(size_t ix)const{ return _data[ix]; }
+	char& getByIndex(size_t ix){ return _data[ix]; }
+
+	size_t _w, _h;
+	vector<char> _data;
+};
+
+ostream& operator<<(ostream& out, const Map& m);
+
+
+#endif /* COGNITEAM_PATHPLANNING_MAP_H_ */
---
+++ C31_PathPlanner/src/Tasks.hpp
@@ -0,0 +1,163 @@
+#ifndef _PATH_PLANNING_SERVER_H_
+#define _PATH_PLANNING_SERVER_H_
+
+
+#include <actionlib/server/simple_action_server.h>
+#include <C0_RobilTask/RobilTask.h>
+#include <C0_RobilTask/RobilTaskAction.h>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+using namespace C0_RobilTask;
+using namespace RobilTask;
+
+class PathPlanningServer{
+
+    typedef RobilTaskGoalConstPtr GOAL;
+    typedef RobilTaskFeedback FEEDBACK;
+    typedef RobilTaskResult RESULT;
+    typedef actionlib::SimpleActionServer<RobilTaskAction> Server;
+    
+protected:
+    ros::NodeHandle _node;
+    Server _server;
+    string _name;
+    FEEDBACK _feedback;
+    RESULT _result;
+
+public:
+    PathPlanningServer(string name = "/PathPlanning"):
+        _server(_node, name, boost::bind(&PathPlanningServer::task, this, _1), false),
+        _name(name)
+    {
+        _server.start();
+        ROS_INFO("instance of %s started.",_name.c_str());
+    }
+
+    void finish(const int32_t& success, const string& plan){
+    	if(success <= 0)
+		{
+			_result.success = success;
+			ROS_INFO("%s: Succeeded", _name.c_str());
+			if(success == PLAN){
+				ROS_INFO("%s: New plan", _name.c_str());
+				_result.plan = plan;
+			}
+			_server.setSucceeded(_result);
+		}else{
+			ROS_INFO("%s: Aborted", _name.c_str());
+			_server.setAborted(_result);
+		}
+    }
+
+    void task(const GOAL &goal){
+        int32_t success = PLAN;
+        string plan ="";
+
+        /* GET TASK PARAMETERS */
+        ROS_INFO("%s: Start: task name = %s", _name.c_str(), goal->name.c_str());
+        ROS_INFO("%s: Start: task id = %s", _name.c_str(), goal->uid.c_str());
+        ROS_INFO("%s: Start: task params = %s", _name.c_str(), goal->parameters.c_str());
+
+        /* HERE PROCESS TASK PARAMETERS */
+
+        /* NUMBER OF ITERATIONS IN TASK LOOP */
+        for(int times =0; times < 100; times++){
+            if (_server.isPreemptRequested() || !ros::ok()){
+
+                /* HERE PROCESS PREEMPTION OR INTERAPT */
+
+                ROS_INFO("%s: Preempted", _name.c_str());
+                _server.setPreempted();
+                success = FAULT;
+                break;
+            }
+
+            /* HERE PROCESS TASK */
+
+            /* SLEEP BETWEEN LOOP ITERATIONS */
+            boost::this_thread::sleep(boost::posix_time::millisec(100));
+        }
+
+        finish( success, plan );
+    }
+
+};
+
+class PathPlanningFocusServer{
+
+    typedef RobilTaskGoalConstPtr GOAL;
+    typedef RobilTaskFeedback FEEDBACK;
+    typedef RobilTaskResult RESULT;
+    typedef actionlib::SimpleActionServer<RobilTaskAction> Server;
+
+protected:
+    ros::NodeHandle _node;
+    Server _server;
+    string _name;
+    FEEDBACK _feedback;
+    RESULT _result;
+
+public:
+    PathPlanningFocusServer(string name = "/PathPlanningFocus"):
+        _server(_node, name, boost::bind(&PathPlanningFocusServer::task, this, _1), false),
+        _name(name)
+    {
+        _server.start();
+        ROS_INFO("instance of %s started.",_name.c_str());
+    }
+
+    void finish(const int32_t& success, const string& plan){
+    	if(success <= 0)
+		{
+			_result.success = success;
+			ROS_INFO("%s: Succeeded", _name.c_str());
+			if(success == PLAN){
+				ROS_INFO("%s: New plan", _name.c_str());
+				_result.plan = plan;
+			}
+			_server.setSucceeded(_result);
+		}else{
+			ROS_INFO("%s: Aborted", _name.c_str());
+			_server.setAborted(_result);
+		}
+    }
+
+    void task(const GOAL &goal){
+        int32_t success = PLAN;
+        string plan ="";
+        
+        /* GET TASK PARAMETERS */
+        ROS_INFO("%s: Start: task name = %s", _name.c_str(), goal->name.c_str());
+        ROS_INFO("%s: Start: task id = %s", _name.c_str(), goal->uid.c_str());
+        ROS_INFO("%s: Start: task params = %s", _name.c_str(), goal->parameters.c_str());
+        
+        /* HERE PROCESS TASK PARAMETERS */
+
+        /* NUMBER OF ITERATIONS IN TASK LOOP */
+        for(int times =0; times < 100; times++){
+            if (_server.isPreemptRequested() || !ros::ok()){
+            
+                /* HERE PROCESS PREEMPTION OR INTERAPT */
+            
+                ROS_INFO("%s: Preempted", _name.c_str());
+                _server.setPreempted();
+                success = FAULT;
+                break;
+            }
+            
+            /* HERE PROCESS TASK */
+
+            /* SLEEP BETWEEN LOOP ITERATIONS */
+            boost::this_thread::sleep(boost::posix_time::millisec(100));
+        }
+
+        finish( success, plan );
+    }
+
+};
+
+
+
+#endif //_PATH_PLANNING_SERVER_H_
