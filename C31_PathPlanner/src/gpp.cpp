7e41c7169b9114a2abff662b07f54404e8dde0c0
danerde <danerde@gmail.com>
2013-01-06 18:50:43 +0200

    path planning (not finished)

--- C31_PathPlanner/src/gpp.cpp
+++ C31_PathPlanner/src/gpp.cpp
@@ -2,6 +2,11 @@
 #include <ros/ros.h>
 #include <C31_PathPlanner/PathPlan.h>
 
+#include "cogniteam_pathplanning.h"
+#include "ConvertorC22.hpp"
+
+#include "Tasks.hpp"
+
 bool PathPlan( C31_PathPlanner::PathPlanRequest& req, C31_PathPlanner::PathPlanResponse& res){
   ROS_INFO("START CALCULATION OF GLOBAL PATH PLANNER");
 
@@ -16,6 +21,12 @@ int main(int argc, char** argv){
 
   ros::spin();
   return 0;
+}
+
+int _main(int argc, char** argv){
+
+	return cogniteam_pathplanning_test_map_inflation(argc, argv);
+
 }
 
 
