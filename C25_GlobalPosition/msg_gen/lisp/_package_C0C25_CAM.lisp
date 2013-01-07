610f21e2497636aa5464f23e58ca3120719c2f1e
root <root@lar2-desktop.(none)>
2013-01-07 08:44:49 +0200

    removing build files

--- C25_GlobalPosition/msg_gen/lisp/_package_C0C25_CAM.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package C25_GlobalPosition-msg)
-(cl:export '(FRAMERATEPERSEC-VAL
-          FRAMERATEPERSEC
-))
--- C25_GlobalPosition/msg_gen/lisp/_package_C25C0_ROP.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package C25_GlobalPosition-msg)
-(cl:export '(IMU-VAL
-          IMU
-          POSE-VAL
-          POSE
-))
