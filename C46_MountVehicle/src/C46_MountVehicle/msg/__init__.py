536b918e83cc146a602ee2726b0645dba9f94f13
Dudi Mass <dudimass@gmail.com>
2013-01-17 12:03:49 +0200

    Working module for 1.3

--- C46_MountVehicle/src/C46_MountVehicle/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._MountFeedback import *
-from ._MountActionFeedback import *
-from ._MountActionResult import *
-from ._MountGoal import *
-from ._MountAction import *
-from ._MountActionGoal import *
-from ._MountResult import *
