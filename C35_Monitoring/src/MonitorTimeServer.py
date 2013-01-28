a2df2e588a1edf055c5ea3696294d567c6943251
raznis <raznissim@gmail.com>
2013-01-28 16:11:23 +0200

    added measurement of time for all nodes. There is a big problem since we don't get the begin messages for all node, and therefore a default start time is used.

--- C35_Monitoring/src/MonitorTimeServer.py
+++ C35_Monitoring/src/MonitorTimeServer.py
@@ -27,27 +27,31 @@ class MonitorTimeServer(object):
 	_monitored_node_id = ""
 	_monitored_task_finished_on_time = False
 	_offline_computed_BT = None
+	_node_start_times_by_id = {}
       
      
 	def callback(self, data):
 		#rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
 		code = data.data[data.data.find("code=")+5]
 		node_id = data.data[data.data.find("[id=")+4 : data.data.find("]", data.data.find("[id=")+4)]
-		print "In callback, monitored node is:%s" % self._monitored_node_id
+		current_time = time.time()
 		if code == "1":		#This means a task is over
-			print "Node with id=%s has ended." % node_id
-			if self._monitored_node_id == node_id:
-				print "The node we are monitoring has ended! Time:%f" % (time.time() - self._start_time)
+			start_time = self._node_start_times_by_id.get(node_id, self._start_time)     #this assigns start_time with default value of the monitor's start time if the start message for this node did not arrive.
+			print "Node with id=%s has ended after %f seconds" % (node_id, current_time - start_time)
+			if self._monitored_node_id == node_id:    
+				print "The node we are monitoring has ended! Time:%f" % (current_time - start_time)
 				self._monitored_task_finished_on_time = True
 		else:			#This means a task has started
 			print "Node with id=%s has begun." % node_id
-			if self._monitored_node_id == node_id:
-				self._start_time = time.time()     
+			self._node_start_times_by_id[node_id] = time.time()
+   			print self._node_start_times_by_id
+			if self._monitored_node_id == node_id: 
+				print "The node we are monitoring has started! Time:%f" % self._node_start_times_by_id.get(node_id)
 
      	def __init__(self,event_file):
 		print "TIME MONITOR SERVER STARTED"
 		#self.listOfTimes=[]
-
+		rospy.Subscriber("/executer/stack_stream", String, self.callback)
 		print "Behavior tree taken from: %s" % sys.argv[0][0:-11]+event_file		
 		self._offline_computed_BT = xmlTree(sys.argv[0][0:-11]+event_file)
   
@@ -79,6 +83,7 @@ class MonitorTimeServer(object):
 	def task(self, goal):
 		print "START MONITORING OF TASK"
 		self._start_time = time.time()
+		rospy.Subscriber("/executer/stack_stream", String, self.callback)
 		print self._start_time
 		#### GET TASK PARAMETERS ####
 		rospy.loginfo("%s: Start: task name = %s",self._action_name, goal.name);
@@ -92,24 +97,23 @@ class MonitorTimeServer(object):
 #		task_result = TASK_RESULT_OK
 		task_plan = ""
 		threshold_time = DEFAULT_THRESHOLD_TIME
-		#TODO - get the average completion time of the node we need to monitor - 
+
            	#from the xml tree get a wrappedNode by it's id/ name or a number (whatever we choose earlier in the constructor)
 		MonitoredNode = self._offline_computed_BT.getWrappedNode(self._monitored_node_id)
 
            	# if the node existes-- suppose to be always true- unless we're not consistent with the event- get the time.      
            	if MonitoredNode!=None:
 			#TODO - we are currently calling getAverageSuccTime with index = 0. Change to handle parameters of the world more robustly.
-			threshold_time = MonitoredNode.getAverageSuccTime(0)
-			print "Monitored Node is:%s, with avg. success time:%f" % (MonitoredNode.getAttrib("name"),threshold_time)
+			#TODO - Currently using 130% of average time as threshold.  
+			threshold_time = MonitoredNode.getAverageSuccTime(0) * 1.3
+			print "Monitored Node is:%s, with threshold time:%f" % (MonitoredNode.getAttrib("name"),threshold_time)
 		else:
 			print "ERROR: Can't find monitored node with id:%s" % self._monitored_node_id
 
-		
-		#rospy.init_node('stack_stream_listener', anonymous=True)
-		rospy.Subscriber("/executer/stack_stream", String, self.callback)
+
     		
 		
-           	while not rospy.is_shutdown() and time.time() - self._start_time  < threshold_time and not self._monitored_task_finished_on_time:
+           	while not rospy.is_shutdown() and time.time() - self._node_start_times_by_id.get(self._monitored_node_id, self._start_time)  < threshold_time and not self._monitored_task_finished_on_time:
 	            	rospy.sleep(0.2)
 		if self._monitored_task_finished_on_time:
 			print "Monitored node finished on time! Finishing monitoring task with SUCCESS."
