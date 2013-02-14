69302f8e63b6a25a1b755217979990c3b037d4da
alexev <alexev@tx.technion.ac.il>
2013-02-14 15:48:49 +0200

    first steps
    Signed-off-by: alexev <alexev@tx.technion.ac.il>

---
+++ C42_DynamicLocomotion/hrl_kinematics/src/hrl_kinematics/msg/__init__.py
@@ -0,0 +1,2 @@
+from ._pCoM_err_msg import *
+from ._CoM_Array_msg import *
---
+++ C42_DynamicLocomotion/zmp_walk/src/zmp_walk/msg/__init__.py
@@ -0,0 +1,4 @@
+from ._Position import *
+from ._Orientation import *
+from ._Pos_and_Ori import *
+from ._walking_trajectory import *
