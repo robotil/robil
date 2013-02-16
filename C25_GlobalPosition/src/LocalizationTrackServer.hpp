545d7e570ace3596a9574f286b65dc2cba701938
root <root@lar0-laptop.(none)>
2013-02-16 23:49:28 +0200

    a fix for C25 things should work now

---
+++ C25_GlobalPosition/src/LocalizationTrackServer.hpp
@@ -0,0 +1,39 @@
+
+#include <actionlib/server/simple_action_server.h>
+#include <C0_RobilTask/RobilTask.h>
+#include <C0_RobilTask/RobilTaskAction.h>
+
+using namespace std;
+using namespace C0_RobilTask;
+
+class LocalizationTrackServer: public RobilTask{
+public:
+    LocalizationTrackServer():
+        RobilTask("/LocalizationTrack")
+    {
+       
+    }
+
+     TaskResult task(const string& name, const string& uid, Arguments& args){
+       /* HERE PROCESS TASK PARAMETERS */
+	    
+	while(true){
+		if (isPreempt()){
+
+			/* HERE PROCESS PREEMPTION OR INTERAPT */
+
+			return TaskResult::Preempted();
+		}
+		 /* HERE PROCESS TASK */
+
+		 ROS_INFO(STR(_name<<": in progress..."));
+		 
+		 //ON SUCCESS : return TaskResult(SUCCESS, "Alright");
+		 //ON FALURE  : return TaskResult(FAULT, "Some problem detected");
+		 
+		 /* SLEEP BETWEEN LOOP ITERATIONS */
+		sleep(100);
+	}
+	return TaskResult::FAULT();
+    }
+};
