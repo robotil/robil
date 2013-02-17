2ae0d69bf805b44a08635c5003bd020ceb05f7aa
Michele Hallak <mhallak@xicore.net>
2013-02-17 12:58:31 +0200

    Good designer and more

--- C0_Scripts/objectRec.sh
+++ C0_Scripts/objectRec.sh
@@ -2,7 +2,7 @@
 #This script should run the modules needed for testing tasks needing path
 
 CURR_WD=$PWD
-PKILL=$CURR_WD'/.killwithpath.txt'
+PKILL=$CURR_WD'/.killwithobjectrec.txt'
 rm $PKILL
 #roslaunch atlas_utils atlas_golf_cart_fire_hose.launch
 #rostopic pub /drc_world/robot_enter_car geometry_msgs/Pose "position (press TAB)
@@ -16,15 +16,13 @@ rm $PKILL
 #C22_PID=$!
 #echo $C22_PID
 #sleep 3
-echo "Starting C31"
-rosrun C31_PathPlanner gpp&
-C31_PID=$!
-echo $C31_PID
+echo "Starting C23"
+rosrun C23_ObjectRecognition c23 /multisense_sl/camera/left/image_color /multisense_sl/camera/right/image_color
+C23_PID=$!
+echo $C23_PID
 echo "Done "
-echo $C31_PID  >> $PKILL
+echo $C23_PID  >> $PKILL
 #echo $C22_PID $C31_PID $C34_PID >> $PKILL
 
-rosservice call executer/run T /userhome/mhallak/git/robil/C0_Scripts/pln
-rosservice call executer/resume T
-rosservice call executer/run T1 /userhome/mhallak/git/robil/C0_Scripts/pln1
-rosservice call executer/resume T1
+rosservice call executer/run O /userhome/mhallak/git/robil/C34_Designer/plans/trackObject.xml
+rosservice call executer/resume O
