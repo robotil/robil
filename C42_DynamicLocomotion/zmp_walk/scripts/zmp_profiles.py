0eddc43647889f63973794d115f96295324189d5
Yuval <yuvals@tx.technion.ac.il>
2013-03-01 14:22:51 +0200

    add transition Min jerk to ZMP profiles
    Signed-off-by: Yuval <yuvals@tx.technion.ac.il>

--- C42_DynamicLocomotion/zmp_walk/scripts/zmp_profiles.py
+++ C42_DynamicLocomotion/zmp_walk/scripts/zmp_profiles.py
@@ -31,14 +31,35 @@ import rospy, sys, os.path
 from pylab import *
 
 # min jerk functions - added by Israel 24.2
-def  transition_Min_jerk(x0,xf, number_of_samples, sample_time):
-    
+def  transition_Min_jerk(x0,xf, number_of_samples): #, sample_time):
+    # x0,xf - are the starting and ending point position of the transition, accordingly
     samples_array = linspace(0,number_of_samples-1, number_of_samples) #- floor(float(number_of_samples)/2)
-    s = samples_array * sample_time
-    s1 = s/max(s)
+    #s = samples_array * sample_time
+    s1 = samples_array/(number_of_samples-1) # s/max(s)
     min_jerk_profile = x0 + (x0 -xf)*( 15*(s1)**4 - 6*(s1)**5 -10*(s1)**3 )
 
     return (min_jerk_profile)
+
+#!!! transition_Min_jerk(x0,xf,N) = r_[ transition_Min_jerk_firstHalf(x0,xm, floor(N/2), N) , transition_Min_jerk_secondHalf(xm,xf, N-floor(N/2), N) ] !!!
+
+def  transition_Min_jerk_firstHalf(x0,xm, samples_in_half_trans, samples_in_whole_trans):
+    # x0,xm - values of the resulting array start from x0 and end at xm  
+    xf = 2.0*xm - x0
+    samples_array = linspace(0,samples_in_half_trans-1, samples_in_half_trans) 
+    s1 = samples_array/(samples_in_whole_trans-1)
+    half_min_jerk_profile = x0 + (x0 - xf)*( 15*(s1)**4 - 6*(s1)**5 -10*(s1)**3 )
+
+    return (half_min_jerk_profile)
+
+def  transition_Min_jerk_secondHalf(xm,xf, samples_in_half_trans, samples_in_whole_trans):
+    # xm,xf - values of the resulting array start from xm and end at xf
+    x0 = 2.0*xm - xf
+    samples_in_first_half_trans = samples_in_whole_trans - samples_in_half_trans
+    samples_array = linspace( samples_in_first_half_trans, samples_in_whole_trans-1, samples_in_half_trans ) 
+    s1 = samples_array/(samples_in_whole_trans-1)
+    half_min_jerk_profile = x0 + (x0 - xf)*( 15*(s1)**4 - 6*(s1)**5 -10*(s1)**3 ) 
+
+    return (half_min_jerk_profile)
 
 def transition_Min_jerk_via_point(x0,xm,xf, number_of_samples, sample_time):
 
@@ -149,9 +170,10 @@ def  Start_sagital_x(ZMP_start_pos, step_length, trans_ratio_of_step, trans_slop
     p_ref0_0x = ZMP_start_pos*ones( samples_in_step + samples_in_start_step_trans + samples_in_step_without_trans )
     
     # step 2 end: transition
-    half_sigmoid_x = transitionSigmoid_firstHalf(step_length/2 - ZMP_start_pos, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    #half_trans_x = ZMP_start_pos + transitionSigmoid_firstHalf(step_length/2 - ZMP_start_pos, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    half_trans_x = transition_Min_jerk_firstHalf( ZMP_start_pos, step_length/2, samples_in_end_step_trans, samples_in_transition)
 
-    p_ref_x = r_[ p_ref0_0x , ZMP_start_pos + half_sigmoid_x ]
+    p_ref_x = r_[ p_ref0_0x , half_trans_x ]
 
     return (p_ref_x)
 
@@ -170,15 +192,17 @@ def  Step_forward_x(ZMP_start_pos, step_length, trans_ratio_of_step, trans_slope
     half_step_length = step_length/2
 
     # step begin : transition
-    half_sigmoid_x_begin = transitionSigmoid_secondHalf( half_step_length - ZMP_start_pos, trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    # half_trans_x_begin = ZMP_start_pos + transitionSigmoid_secondHalf( half_step_length - ZMP_start_pos, trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    half_trans_x_begin = transition_Min_jerk_secondHalf( ZMP_start_pos, half_step_length, samples_in_start_step_trans, samples_in_transition)
 
     # step middle: with out transitions
     p_ref1x = half_step_length*ones( samples_in_step_without_trans )
     
     # step 2 end: transition
-    half_sigmoid_x_end = transitionSigmoid_firstHalf(half_step_length, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    #half_trans_x_end = transitionSigmoid_firstHalf(half_step_length, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    half_trans_x_end = transition_Min_jerk_firstHalf(0, half_step_length, samples_in_end_step_trans, samples_in_transition)
 
-    p_ref_x = r_[ ZMP_start_pos + half_sigmoid_x_begin, p_ref1x , half_sigmoid_x_end + half_step_length ]
+    p_ref_x = r_[ half_trans_x_begin, p_ref1x , half_trans_x_end + half_step_length ]
 
     return (p_ref_x)
 
@@ -197,12 +221,13 @@ def  Stop_sagital_x(ZMP_start_pos, step_length, trans_ratio_of_step, trans_slope
     half_step_length = step_length/2
 
     # step 1 begin : transition
-    half_sigmoid_x_begin = transitionSigmoid_secondHalf( half_step_length - ZMP_start_pos, trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    # half_trans_x_begin = ZMP_start_pos + transitionSigmoid_secondHalf( half_step_length - ZMP_start_pos, trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    half_trans_x_begin = transition_Min_jerk_secondHalf( ZMP_start_pos, half_step_length, samples_in_start_step_trans, samples_in_transition)
     
     # step 1 middle to step 2 end: no movement
     p_ref1x = half_step_length*ones( samples_in_step_without_trans + samples_in_end_step_trans + samples_in_step )
 
-    p_ref_x = r_[ ZMP_start_pos + half_sigmoid_x_begin , ZMP_start_pos + p_ref1x ]
+    p_ref_x = r_[ half_trans_x_begin , ZMP_start_pos + p_ref1x ]
 
     return (p_ref_x)
 
@@ -237,17 +262,17 @@ def  Start_lateral_y_weight_to_left_foot(ZMP_start_pos, step_width, trans_ratio_
 
     # step 1 to step 2 transition:
     samples_in_trans = samples_in_end_step_trans + samples_in_start_step_trans
-    sigmoid_y_small_step = transitionSigmoid( step_width/2 - ZMP_start_pos, trans_slope_steepens_factor, samples_in_trans, sample_time )
+    #trans_y_small_step = ZMP_start_pos + transitionSigmoid( step_width/2 - ZMP_start_pos, trans_slope_steepens_factor, samples_in_trans, sample_time )
+    trans_y_small_step = transition_Min_jerk( ZMP_start_pos, step_width/2, samples_in_trans)
     
     # step 2 middle: with out transitions
     p_ref1y = step_width/2*ones( samples_in_step_without_trans )
 
     # step 2 end: transition
-    half_sigmoid_y = transitionSigmoid_firstHalf(step_width/2, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    # half_trans_y = transitionSigmoid_firstHalf(step_width/2, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    half_trans_y = transition_Min_jerk_firstHalf(0,step_width/2, samples_in_end_step_trans, samples_in_transition)
 
-
-
-    p_ref_y = r_[ p_ref0_0y , ZMP_start_pos + sigmoid_y_small_step, p_ref1y, step_width/2 - half_sigmoid_y]
+    p_ref_y = r_[ p_ref0_0y , trans_y_small_step, p_ref1y, step_width/2 - half_trans_y]
 
     return (p_ref_y)
 
@@ -264,17 +289,17 @@ def  Step_onto_right_foot(ZMP_start_pos, step_width, trans_ratio_of_step, trans_
     # total number of samples = samples_in_step
 
     # step begin : transition
-    half_sigmoid_y_begin = transitionSigmoid_secondHalf(-(step_width/2 + ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    #half_trans_y_begin = ZMP_start_pos + transitionSigmoid_secondHalf(-(step_width/2 + ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    half_trans_y_begin = transition_Min_jerk_secondHalf( ZMP_start_pos,-(step_width/2), samples_in_start_step_trans, samples_in_transition)
     
     # step middle: with out transitions
     p_ref1y = -step_width/2*ones( samples_in_step_without_trans )
 
     # step 2 end: transition
-    half_sigmoid_y = transitionSigmoid_firstHalf(step_width/2, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    # half_trans_y = transitionSigmoid_firstHalf(step_width/2, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    half_trans_y = transition_Min_jerk_firstHalf(0,step_width/2, samples_in_end_step_trans, samples_in_transition)
 
-
-
-    p_ref_y = r_[ ZMP_start_pos + half_sigmoid_y_begin , p_ref1y, half_sigmoid_y - step_width/2 ]
+    p_ref_y = r_[ half_trans_y_begin , p_ref1y, half_trans_y - step_width/2 ]
 
     return (p_ref_y)
 
@@ -292,17 +317,17 @@ def  Step_onto_left_foot(ZMP_start_pos, step_width, trans_ratio_of_step, trans_s
     # total number of samples = samples_in_step
 
     # step begin : transition
-    half_sigmoid_y_begin = transitionSigmoid_secondHalf( (step_width/2 - ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    # half_trans_y_begin = ZMP_start_pos + transitionSigmoid_secondHalf( (step_width/2 - ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    half_trans_y_begin = transition_Min_jerk_secondHalf( ZMP_start_pos, step_width/2, samples_in_start_step_trans, samples_in_transition)
     
     # step middle: with out transitions
     p_ref1y = step_width/2*ones( samples_in_step_without_trans )
 
     # step 2 end: transition
-    half_sigmoid_y = transitionSigmoid_firstHalf(step_width/2, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    # half_trans_y = transitionSigmoid_firstHalf(step_width/2, trans_slope_steepens_factor, samples_in_end_step_trans, sample_time)
+    half_trans_y = transition_Min_jerk_firstHalf( 0, step_width/2, samples_in_end_step_trans, samples_in_transition)
 
-
-
-    p_ref_y = r_[ ZMP_start_pos + half_sigmoid_y_begin , p_ref1y, step_width/2 - half_sigmoid_y ]
+    p_ref_y = r_[ half_trans_y_begin , p_ref1y, step_width/2 - half_trans_y ]
 
     return (p_ref_y)
 
@@ -321,7 +346,8 @@ def  Stop_lateral_y_from_left_foot(ZMP_start_pos, step_width, trans_ratio_of_ste
 
     ## Step onto right foot:
     # step 1 begin : transition
-    half_sigmoid_y_begin = transitionSigmoid_secondHalf(-(step_width/2 + ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    # half_trans_y_begin = ZMP_start_pos + transitionSigmoid_secondHalf(-(step_width/2 + ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    half_trans_y_begin = transition_Min_jerk_secondHalf( ZMP_start_pos,-(step_width/2), samples_in_start_step_trans, samples_in_transition)
     
     # step 1 middle: with out transitions
     p_ref1y = -step_width/2*ones( samples_in_step_without_trans )
@@ -329,14 +355,15 @@ def  Stop_lateral_y_from_left_foot(ZMP_start_pos, step_width, trans_ratio_of_ste
     ## move and stay at double support:
     # step 1 to step 2 transition:
     samples_in_trans = samples_in_end_step_trans + samples_in_start_step_trans
-    sigmoid_y_small_step = transitionSigmoid( step_width/2 - ZMP_start_pos, trans_slope_steepens_factor, samples_in_trans, sample_time )
+    # trans_y_small_step = transitionSigmoid( step_width/2 ?(- ZMP_start_pos)?, trans_slope_steepens_factor, samples_in_trans, sample_time )
+    trans_y_small_step = transition_Min_jerk( 0, step_width/2 + ZMP_start_pos, samples_in_trans)
 
     # step 2 middle and end: 
     p_ref2y = zeros( samples_in_step_without_trans + samples_in_end_step_trans )
 
 
 
-    p_ref_y = r_[ ZMP_start_pos + half_sigmoid_y_begin , p_ref1y, sigmoid_y_small_step - step_width/2, p_ref2y ]
+    p_ref_y = r_[ half_trans_y_begin, p_ref1y, trans_y_small_step - step_width/2, p_ref2y ]
 
     return (p_ref_y)
 
@@ -354,7 +381,8 @@ def  Stop_lateral_y_from_right_foot(ZMP_start_pos, step_width, trans_ratio_of_st
 
     ## Step onto right foot:
     # step 1 begin : transition
-    half_sigmoid_y_begin = transitionSigmoid_secondHalf( (step_width/2 - ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    # half_trans_y_begin = ZMP_start_pos + transitionSigmoid_secondHalf( (step_width/2 - ZMP_start_pos), trans_slope_steepens_factor, samples_in_start_step_trans, sample_time)
+    half_trans_y_begin = transition_Min_jerk_secondHalf( ZMP_start_pos, step_width/2, samples_in_start_step_trans, samples_in_transition)
     
     # step 1 middle: with out transitions
     p_ref1y = step_width/2*ones( samples_in_step_without_trans )
@@ -362,14 +390,14 @@ def  Stop_lateral_y_from_right_foot(ZMP_start_pos, step_width, trans_ratio_of_st
     ## move and stay at double support:
     # step 1 to step 2 transition:
     samples_in_trans = samples_in_end_step_trans + samples_in_start_step_trans
-    sigmoid_y_small_step = transitionSigmoid( -(step_width/2 + ZMP_start_pos), trans_slope_steepens_factor, samples_in_trans, sample_time )
+    trans_y_small_step = transition_Min_jerk( 0,-(step_width/2 - ZMP_start_pos), samples_in_trans)
 
     # step 2 middle and end: 
     p_ref2y = zeros( samples_in_step_without_trans + samples_in_end_step_trans )
 
 
 
-    p_ref_y = r_[ ZMP_start_pos + half_sigmoid_y_begin , p_ref1y, step_width/2 + sigmoid_y_small_step, p_ref2y ]
+    p_ref_y = r_[ half_trans_y_begin , p_ref1y, step_width/2 + trans_y_small_step, p_ref2y ]
 
     return (p_ref_y)
 
