888d1928e83a3ffc1ffe9632e274249b1e900472
yakirari <yakirari@gmail.com>
2013-04-24 19:21:31 +0300

    integration with IAI

---
+++ C34_BTExecuter/bin_so/src/BT.d
@@ -0,0 +1,8 @@
+src/BT.d: ../src/BT.cpp ../src/BT.h ../src/Logger.h \
+ ../src/StringOperations.h
+
+../src/BT.h:
+
+../src/Logger.h:
+
+../src/StringOperations.h:
---
+++ C34_BTExecuter/bin_so/src/ExeStack.d
@@ -0,0 +1,5 @@
+src/ExeStack.d: ../src/ExeStack.cpp ../src/ExeStack.h ../src/Logger.h
+
+../src/ExeStack.h:
+
+../src/Logger.h:
