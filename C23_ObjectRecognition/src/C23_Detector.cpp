82dd92febbf0898176863710a29817b020428778
Majd Srour <isl@ubuntu.ubuntu-domain>
2013-04-25 12:25:17 +0300

    Detecting the gate in all directions, even if part of it is seen. We only detect "forward" gates, meaning red on the right side and blue is on the left.

--- C23_ObjectRecognition/src/C23_Detector.cpp
+++ C23_ObjectRecognition/src/C23_Detector.cpp
@@ -192,7 +192,7 @@ Mat fromSensorMsg(const sensor_msgs::ImageConstPtr& msg)
 		static pcl::PointXYZ leftC;
 		static pcl::PointXYZ rightC;
     bool res = false;
-
+    bool left=false, right=false;
 		IplImage* img = new IplImage(srcImg);
 		IplImage* imgHSV = cvCreateImage(cvGetSize(img), 8, 3);
 
@@ -209,33 +209,38 @@ Mat fromSensorMsg(const sensor_msgs::ImageConstPtr& msg)
 		//contoursR
 		Mat bw,gray;
 
-
+  //  ROS_INFO("1");
 		////finding center mass of right column
 		threshold(threshMat, bw, 10, 255, CV_THRESH_BINARY);
 		vector<vector<cv::Point> > contoursR;
 		findContours(bw, contoursR, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-		drawContours(srcImg, contoursR, -1, CV_RGB(255,0,0), 2);
+		vector<Moments> muR(contoursR.size() );
+		vector<Point2f> mcR(contoursR.size() );
+		int biggstR=0;
+		if(contoursR.size() != 0) {
+		  drawContours(srcImg, contoursR, -1, CV_RGB(255,0,0), 2);
+     
+		  /// Get the moments and mass centers:
+		   
+		    
+		    
+		    for(unsigned int i = 0; i < contoursR.size(); i++ )
+		    {
+			    muR[i] = moments( contoursR[i], false );
+			    mcR[i] = Point2f( muR[i].m10/muR[i].m00 , muR[i].m01/muR[i].m00 );
+			    if (muR[biggstR].m00<=muR[i].m00)
+				    biggstR=i;
 
-		/// Get the moments and mass centers:
-		  vector<Moments> muR(contoursR.size() );
-		  vector<Point2f> mcR(contoursR.size() );
-		  int biggstR=0;
-		  for(unsigned int i = 0; i < contoursR.size(); i++ )
-		  {
-			  muR[i] = moments( contoursR[i], false );
-			  mcR[i] = Point2f( muR[i].m10/muR[i].m00 , muR[i].m01/muR[i].m00 );
-			  if (muR[biggstR].m00<=muR[i].m00)
-				  biggstR=i;
+		    }
 
-		  }
+		    circle( srcImg, mcR[biggstR], 16, 60, -1, 8, 0 );
+        right = true;
+		    if (pclcloud.at(mcR[biggstR].x,mcR[biggstR].y).x<50 && pclcloud.at(mcR[biggstR].x,mcR[biggstR].y).y <50 && pclcloud.at(mcR[biggstR].x,mcR[biggstR].y).x !=0)
+		   {
+			  rightC=pclcloud.at(mcR[biggstR].x,mcR[biggstR].y);
 
-		  circle( srcImg, mcR[biggstR], 16, 60, -1, 8, 0 );
-
-		  if (pclcloud.at(mcR[biggstR].x,mcR[biggstR].y).x<50 && pclcloud.at(mcR[biggstR].x,mcR[biggstR].y).y <50 && pclcloud.at(mcR[biggstR].x,mcR[biggstR].y).x !=0)
-		 {
-			rightC=pclcloud.at(mcR[biggstR].x,mcR[biggstR].y);
-
-		  }
+		    }
+	  }
 
 
 		  /////finding center mass of left column
@@ -247,65 +252,136 @@ Mat fromSensorMsg(const sensor_msgs::ImageConstPtr& msg)
 		  vector<vector<cv::Point> > contoursL;
 	//	  cout<<"next2"<<endl;
 		  findContours(bwL, contoursL, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-		  drawContours(srcImg, contoursL, -1, CV_RGB(0,0,255), 2);
-
 		  vector<Moments> muL(contoursL.size() );
-		  	  vector<Point2f> mcL(contoursL.size() );
-		  	  int biggstL=0;
-		  	  for(unsigned int i = 0; i < contoursL.size(); i++ )
-		  	  {
-		  		  muL[i] = moments( contoursL[i], false );
-		  		  mcL[i] = Point2f( muL[i].m10/muL[i].m00 , muL[i].m01/muL[i].m00 );
-		  		  if (muL[biggstL].m00<=muL[i].m00)
-		  			  biggstL=i;
+      vector<Point2f> mcL(contoursL.size() );
+   	  int biggstL=0;
+		  if(contoursL.size() !=0) {
+		    drawContours(srcImg, contoursL, -1, CV_RGB(0,0,255), 2);
 
-		  	  }
+		    
+      
+      	  for(unsigned int i = 0; i < contoursL.size(); i++ )
+      	  {
+      		  muL[i] = moments( contoursL[i], false );
+      		  mcL[i] = Point2f( muL[i].m10/muL[i].m00 , muL[i].m01/muL[i].m00 );
+      		  if (muL[biggstL].m00<=muL[i].m00)
+      			  biggstL=i;
 
-		  	  circle( srcImg, mcL[biggstL], 16, Scalar(0,0,255), -1, 8, 0 );
+      	  }
+              left = true;
+		      	  circle( srcImg, mcL[biggstL], 16, Scalar(0,0,255), -1, 8, 0 );
+              if (pclcloud.at(mcL[biggstL].x,mcL[biggstL].y).x<50 && pclcloud.at(mcL[biggstL].x,mcL[biggstL].y).y <50 && pclcloud.at(mcL[biggstL].x,mcL[biggstL].y).x !=0)
+			        {
+				        leftC=pclcloud.at(mcL[biggstL].x,mcL[biggstL].y);
+			        }
+
+        }
+
+         if(left && right) {
+		      	
+
+		      	line( srcImg,mcL[biggstL],mcR[biggstR],Scalar(0,0,0),5,8);
+
+		      //	cout <<"\nleft :"<<leftC.x<<" ,"<<leftC.y<<endl;
+		      //	cout<<"\nright :"<<rightC.x<<" ,"<<rightC.y<<endl;
+
+		      	double gate =sqrt(pow((leftC.x-rightC.x),2)+pow((leftC.y-rightC.y),2));
+		      //	cout<<gate<<endl;
+		      res = false;
+		      	if (gate > 4.5 && gate < 5.8)
+		      	{
+		      		res = true;
+
+		      	}
+          if(!res) {
+            left = leftC.z < rightC.z;
+            right = leftC.z > rightC.z;
+          
+          } else {
+			    //find lines
+			      Mat dst,cdst;
+			     // ROS_INFO("4.4");
+			      Canny(threshMat, dst, 50, 200, 3);
+			     // ROS_INFO("4.5");
+			      cvtColor(dst, cdst, CV_GRAY2BGR);
+          //  ROS_INFO("5");
+			      //Probabilistic Hough Line Transform
+			      vector<Vec4i> lines;
+			        HoughLinesP(dst, lines, 5, CV_PI/180, 50, 100, 10 );
+			        for( size_t i = 0; i < lines.size(); i++ )
+			        {
+				      Vec4i l = lines[i];
+				      line( cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+			        }
+         //   ROS_INFO("6");
+
+			      //cout<<"left"<<mcL[biggstL]<<"right"<<mcR[biggstR]<<endl;
+			      x = (mcL[biggstL].x + mcR[biggstR].x)/2;
+			      y = (mcL[biggstL].y + mcR[biggstR].y)/2;
+
+			      Point2f a((float)x,(float)y);
+			      circle( srcImg, a, 16, Scalar(0,0,255), -1, 8, 0 );
+         //   imshow("TESTING",srcImg);
+         //   waitKey(0);
+            return true;
+          }
+      }
+     
+      cvInRangeS(imgHSV, cvScalar(60,30,30),cvScalar(80,255,255), imgThreshed);//filter blue
 
 
+		  Mat threshMatM(imgThreshed),bwM;
+		  threshold(threshMatM, bwM, 10, 255, CV_THRESH_BINARY);
+		  vector<vector<cv::Point> > contoursM;
+	//	  cout<<"next2"<<endl;
+		  findContours(bwM, contoursM, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+		  vector<Moments> muM(contoursM.size() );
+      vector<Point2f> mcM(contoursM.size() );
+   	  int biggstM=0;
+   	  
+		  if(contoursM.size() !=0) {
+		    drawContours(srcImg, contoursM, -1, CV_RGB(0,0,255), 2);
+       // imshow("TESTING",srcImg);
+      //  waitKey(0);
+		    
+      
+      	  for(unsigned int i = 0; i < contoursM.size(); i++ )
+      	  {
+      		  muM[i] = moments( contoursM[i], false );
+      		  mcM[i] = Point2f( muM[i].m10/muM[i].m00 , muM[i].m01/muM[i].m00 );
+      		  if (muM[biggstM].m00<=muM[i].m00)
+      			  biggstM=i;
 
+      	  }
+         
+		      	  circle( srcImg, mcM[biggstM], 16, Scalar(0,0,255), -1, 8, 0 );
+              if (pclcloud.at(mcM[biggstM].x,mcM[biggstM].y).x<50 && pclcloud.at(mcM[biggstM].x,mcM[biggstM].y).y <50 && pclcloud.at(mcM[biggstM].x,mcM[biggstM].y).x !=0)
+			        {
+				    //    leftM=pclcloud.at(mcM[biggstM].x,mcM[biggstM].y);
+			        }
+          if(right) {
+            
+            x = mcR[biggstR].x + (mcM[biggstM].y - mcR[biggstR].y);
+            y =  mcR[biggstR].y;
+          //  cout << "Detected right" << endl;
+            circle( srcImg, Point2f(x,y), 16, Scalar(0,0,255), -1, 8, 0 );
+          //   imshow("Testing" , srcImg);
+          //   waitKey(0);
+            return mcM[biggstM].x < mcR[biggstR].x ? true : false;
+          
+          } else {
+            x = mcL[biggstL].x - (mcM[biggstM].y - mcL[biggstL].y);
+            y =  mcL[biggstL].y;
+           //  cout << "Detected left" << endl;
+             circle( srcImg, Point2f(x,y), 16, Scalar(0,0,255), -1, 8, 0 );
+          //  imshow("Testing" , srcImg);
+           // waitKey(0);
+            return mcM[biggstM].x > mcL[biggstL].x ? true : false;
 
-		  	 if (pclcloud.at(mcL[biggstL].x,mcL[biggstL].y).x<50 && pclcloud.at(mcL[biggstL].x,mcL[biggstL].y).y <50 && pclcloud.at(mcL[biggstL].x,mcL[biggstL].y).x !=0)
-			 {
-				leftC=pclcloud.at(mcL[biggstL].x,mcL[biggstL].y);
-			  }
-
-		  	line( srcImg,mcL[biggstL],mcR[biggstR],Scalar(0,0,0),5,8);
-
-		  //	cout <<"\nleft :"<<leftC.x<<" ,"<<leftC.y<<endl;
-		  //	cout<<"\nright :"<<rightC.x<<" ,"<<rightC.y<<endl;
-		  	double gate =sqrt(pow((leftC.x-rightC.x),2)+pow((leftC.y-rightC.y),2));
-		  //	cout<<gate<<endl;
-		  	if (gate > 4.7 && gate < 5.2)
-		  	{
-		  		res = true;
-		  	}
-
-			//find lines
-			Mat dst,cdst;
-			Canny(threshMat, dst, 50, 200, 3);
-			cvtColor(dst, cdst, CV_GRAY2BGR);
-
-			//Probabilistic Hough Line Transform
-			vector<Vec4i> lines;
-			  HoughLinesP(dst, lines, 5, CV_PI/180, 50, 100, 10 );
-			  for( size_t i = 0; i < lines.size(); i++ )
-			  {
-				Vec4i l = lines[i];
-				line( cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
-			  }
-
-
-			//cout<<"left"<<mcL[biggstL]<<"right"<<mcR[biggstR]<<endl;
-			x = (mcL[biggstL].x + mcR[biggstR].x)/2;
-			y = (mcL[biggstL].y + mcR[biggstR].y)/2;
-
-			Point2f a((float)x,(float)y);
-			circle( srcImg, a, 16, Scalar(0,0,255), -1, 8, 0 );
-
-
-		  return true;
+          }
+        }
+        return false;
+		  
 	
 	}
 
