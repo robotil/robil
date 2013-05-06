28d9f9bfd5666b1509cc6d6aa0c7cc43ccb3fba7
Michele Hallak <mhallak@xicore.net>
2013-05-06 10:28:14 +0300

    For qualification 1

--- C0_Scripts/zmpAvizarim.sh
+++ C0_Scripts/zmpAvizarim.sh
@@ -5,6 +5,12 @@ CURR_WD=$PWD
 PKILL=$CURR_WD'/.killZmpAvizarim.txt'
 rm $PKILL
 echo "This script run some tasks needed to check walk task"
+echo "Falling"
+rosrun C25_AtlasFell Fell_server.py &
+C01=$!
+rosrun C25_AtlasFell Fell_client.py &
+C02=$!
+sleep 2
 echo "FallingMonitor"
 rosrun C35_Monitoring FallingMonitor.py &
 C1=$!
@@ -17,8 +23,9 @@ echo "Standup"
 rosrun C48_StandUp C48_StandUp.py &
 C3=$!
 sleep 1
-echo "ZMP"
-roslaunch C42_ZMPWalk zmp_walk.launch &
-C4=$!
-echo $C1 $C2 $C3 $C4 >> $PKILL
+#echo "ZMP"
+#roslaunch C42_ZMPWalk zmp_walk.launch &
+#C4=$!
+echo $C01 $C02 $C1 $C2 $C3  >> $PKILL
+#echo $C1 $C2 $C3 $C4 >> $PKILL
 
