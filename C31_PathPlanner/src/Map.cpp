e627c2ba91f30747d6b3ee379e95e1e9dfd78a1f
danerde <danerde@gmail.com>
2013-05-26 12:47:38 +0300

    -

--- C31_PathPlanner/src/Map.cpp
+++ C31_PathPlanner/src/Map.cpp
@@ -22,7 +22,18 @@ ostream& operator<<(ostream& out, const Map& m){
 	}
 	return out;
 }
-
+ostream& operator<<(ostream& out, const AltMap& m){
+	out<<"  "; for(size_t x=0;x<10;x++){cout<<' '<<x<<' ';}for(size_t x=10;x<m.w();x++){cout<<x<<' ';} out<<endl;
+	for(long y=(long)m.h()-1;y>=0;y--){
+//	for(size_t y=0;y<m.h();y++){
+		if(y<10) out<<' '; out<<y;
+		for(size_t x=0;x<m.w();x++){
+			out<<' '<<m.str(x,y)<<' ';
+		}
+		out<<endl;
+	}
+	return out;
+}
 bool BaseMap::inRange(long x, long y)const{
 	if(x<0||y<0) return false;
 	if(x>=(long)w()||y>=(long)h()) return false;
