097881e6cc9ad47d605d0bec008bb1308c10752d
Michele Hallak <mhallak@xicore.net>
2013-05-28 16:27:49 +0300

    for VRC

---
+++ C0_Scripts/c11Agent.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#This script should start the perception modules
+
+CURR_WD=$PWD
+PKILL=$CURR_WD'/.c11agent.txt'
+rm $PKILL
+
+echo "Running C11_Agent "
+rosrun C11_Agent C11_Agent &
+C11=$!  
+
+echo "Running GPP"
+rosrun C31_PathPlanner gpp | tee gpp.log  &
+C31=$!
+
+echo "Runnung Executer "
+rosrun C34_Executer executer &
+C34=$!  
+
+echo $C11 $C31 $C34 >> $PKILL
+
---
+++ C0_Scripts/comp1.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#This script should start the perception modules
+
+echo "Perception...."
+./perception.sh
+sleep 3
+echo "Standup...."
+./standup.sh
+sleep 3
+echo "Posture Control and Stability"
+#./postCont.sh
+sleep 4
+#echo "Walking"
+#./walking.sh
+#sleep 3
+#echo "Driving"
+#./driving.sh
+#sleep 3
+echo "C11 Agent"
+./c11Agent.sh
+
