19eb11ff5fcbc8c5d5b91d8a3e97949156ef3e41
Gal Lavi <ubuntu@Mafrum.(none)>
2013-06-04 16:46:49 +0300

    changing coordinates for driving and walking monitors

--- C35_Monitoring/src/WalkingMonitor.py
+++ C35_Monitoring/src/WalkingMonitor.py
@@ -28,10 +28,10 @@ class WalkingMonitor(RobilTask):
 		  elapsed_time = rospy.get_time() - WalkingMonitor.init_time
 		  #print "x" ,msg.pose.pose.pose.position.x, WalkingMonitor.init_x
 		  #print "y" ,msg.pose.pose.pose.position.y, WalkingMonitor.init_y
-		  print "time" ,elapsed_time
+		  #print "time" ,elapsed_time
 		  print "delta" ,delta_x, delta_y
 		  
-		  if WalkingMonitor.init_x != -1 and WalkingMonitor.init_y != -1 and elapsed_time > 20 and math.fabs(delta_x) < 0.2 and math.fabs(delta_y) < 0.2:
+		  if WalkingMonitor.init_x != -1 and WalkingMonitor.init_y != -1 and elapsed_time > 20 and math.fabs(delta_x) < 0.1 and math.fabs(delta_y) < 0.1:
 			  WalkingMonitor.detected_problem = True
 	
 	def __init__(self, name):
