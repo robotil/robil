7107f4140cd4440fff131e49bef00ee2918d4bd9
Yuval <yuvals@tx.technion.ac.il>
2013-06-04 09:46:59 +0300

    work in progress, brings robot to table
    Signed-off-by: Yuval <yuvals@tx.technion.ac.il>

--- C42_DynamicLocomotion/scripts/C31_Clone_VRC3_StartingPenToFirehose.py
+++ C42_DynamicLocomotion/scripts/C31_Clone_VRC3_StartingPenToFirehose.py
@@ -6,6 +6,6 @@ rospy.init_node('C31_clone')
 pub = rospy.Publisher('/path',C31_Waypoints)
 rospy.sleep(1)
 wp = C31_Waypoints()
-wp.points = [C31_Location(-3.0,-38.0),C31_Location(4.0,-38.0),C31_Location(4.0,-30.0),C31_Location(0.0,-27.0),C31_Location(0.0,-22.5)]
+wp.points = [C31_Location(-7.0,-15.0),C31_Location(-0.5,-15.0),C31_Location(-1.0,-7.5),C31_Location(-2.0,-5.1)]
 pub.publish(wp)
 rospy.spin()
