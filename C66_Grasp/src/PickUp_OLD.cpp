952001544484c76dd4f8b7e5084dd07f73b7ccbe
lab116-2 <lab116-2@lab116-2.(none)>
2013-06-14 00:51:44 +0300

    good night

--- C66_Grasp/src/PickUp_OLD.cpp
+++ C66_Grasp/src/PickUp_OLD.cpp
@@ -8,7 +8,6 @@
 #include <math.h>
 #include <boost/bind.hpp>
 #include <string>
-#include <C23_dFind/perceptionTransform.h>
 #include <boost/bind/bind.hpp>
 #include <hand_grasp/grasp.h>
 #include <move_hand/pelvis_move_hand.h>
@@ -17,7 +16,6 @@
 #include <sensor_msgs/JointState.h>
 #include <sstream>
 #include <iostream>
-#include <C23_dFind/perceptionTransform.h>
 #include <C23_ObjectRecognition/C23_orient.h>
 #include <sandia_hand_msgs/SimpleGraspSrv.h>
 #include <C67_CarManipulation/arm_path.h>
@@ -43,7 +41,7 @@ void update_comand(){
 class PickUp{
 private:
 	ros::NodeHandle nh_;
-	ros::ServiceClient pelvis_move_hand_matrix_cli_,perception_transform_cli_,pelvis_move_hand_cli_,move_hand_cli_,r_arm_path_cli_;
+	ros::ServiceClient pelvis_move_hand_matrix_cli_,pelvis_move_hand_cli_,move_hand_cli_,r_arm_path_cli_;
 	actionlib::SimpleActionServer<C0_RobilTask::RobilTaskAction> as_; // NodeHandle instance must be created before this line.
 	C0_RobilTask::RobilTaskFeedback feedback_;
 	C0_RobilTask::RobilTaskResult result_;
@@ -62,7 +60,6 @@ public:
 		pelvis_move_hand_matrix_cli_ = nh_.serviceClient<move_hand::matrix>("C66_matrix");
 		pelvis_move_hand_cli_ = nh_.serviceClient<move_hand::pelvis_move_hand>("pelvis_move_hand");
 		move_hand_cli_ = nh_.serviceClient<move_hand::move_hand>("move_hand");
-		perception_transform_cli_ = nh_.serviceClient<C23_dFind::perceptionTransform>("perceptionTransform");
 		r_arm_path_cli_ = nh_.serviceClient<C67_CarManipulation::arm_path>("rPath_srv");
 		sandia_right_pub = rosnode->advertise<sandia_hand_msgs::SimpleGrasp>("/sandia_hands/r_hand/simple_grasp", 100, true);
 		//Set callback functions
@@ -120,12 +117,6 @@ public:
 		double *X,*Y,*Z,*R,*P,*YA;
 		switch (c) {
 		case 'F':
-			/*open hand*/
-			ROS_INFO("opening right hand");
-			/*grasp_msg.strength = 0;
-			pub_hand_grasp_right.publish(grasp_msg);
-			ros::Duration(0.3).sleep();
-			 */
 
 			ros::Duration(0.3).sleep();
 			ROS_INFO("requesting transformation from perception");
@@ -142,8 +133,63 @@ public:
 			sandia_srv.request.grasp.name = "cylindrical";
 			sandia_srv.request.grasp.closed_amount = 0.4;
 			}
-			update_comand();
-			ros::spinOnce();
+
+			arm_path_msg.request.PositionDestination.x = -0.128;
+			arm_path_msg.request.PositionDestination.y = -0.414;
+			arm_path_msg.request.PositionDestination.z = 0.07;
+			arm_path_msg.request.AngleDestination.x = 2.749;
+			arm_path_msg.request.AngleDestination.y = 0.76378;
+			arm_path_msg.request.AngleDestination.z = -1.408;
+			arm_path_msg.request.time =4;
+			arm_path_msg.request.points =200;
+			ROS_INFO("Calibrating Hands1");
+			if(r_arm_path_cli_.call(arm_path_msg)){}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
+
+			arm_path_msg.request.PositionDestination.x = -0.3886;
+			arm_path_msg.request.PositionDestination.y = -0.569;
+			arm_path_msg.request.PositionDestination.z = 0.0;
+			arm_path_msg.request.AngleDestination.x = 2.803;
+			arm_path_msg.request.AngleDestination.y = 0.927;
+			arm_path_msg.request.AngleDestination.z = -1.471;
+			ROS_INFO("Calibrating Hands2");
+			if(r_arm_path_cli_.call(arm_path_msg)){}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
+			arm_path_msg.request.PositionDestination.x = -0.389;
+			arm_path_msg.request.PositionDestination.y = -0.582;
+			arm_path_msg.request.PositionDestination.z = 0.552;
+			arm_path_msg.request.AngleDestination.x = 2.734;
+			arm_path_msg.request.AngleDestination.y = -0.862;
+			arm_path_msg.request.AngleDestination.z = -2.309;
+			ROS_INFO("Calibrating Hands3");
+			if(r_arm_path_cli_.call(arm_path_msg)){}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
+			arm_path_msg.request.PositionDestination.x = 0.3;
+			arm_path_msg.request.PositionDestination.y = -0.4;
+			arm_path_msg.request.PositionDestination.z = 0.4;
+			arm_path_msg.request.AngleDestination.x = 2.762;
+			arm_path_msg.request.AngleDestination.y = 0.5;
+			arm_path_msg.request.AngleDestination.z = 0.0;
+			ROS_INFO("Calibrating Hands4");
+			if(r_arm_path_cli_.call(arm_path_msg)){}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
+
 			if (getObjectData(g,X,Y,Z,R,P,YA)){
 				//Grasp the other way if the grasp is over 90 degrees
 				if (!((-M_PI/2<*YA)&&(*YA<M_PI/2))) {
@@ -162,12 +208,12 @@ public:
 				transform(2,0) = -sp; 	transform(2,1) = cp*sr; 			transform(2,2) = cp*cr; 		 transform(2,3) = *Z;
 				transform(3,0) = 0; 	transform(3,1) = 0; 				transform(3,2) = 0; 			 transform(3,3) = 1;
 
-						sr=sin(-3.13);
-						cr=cos(-3.13);
-						sp=sin(0.097);
-						cp=cos(0.097);
-						sy=sin(0.625);
-						cy=cos(0.625);
+				sr=sin(-3.13);
+				cr=cos(-3.13);
+				sp=sin(0.097);
+				cp=cos(0.097);
+				sy=sin(0.625);
+				cy=cos(0.625);
 
 
 				origin(0,0) =cp*cy; 	origin(0,1) = cy*sp*sr-cr*sy; 	origin(0,2) = sr*sy+cr*cy*sp; origin(0,3) = 0.502;
@@ -212,12 +258,12 @@ public:
 			arm_path_msg.request.PositionDestination.z -= 0.2;
 			if(r_arm_path_cli_.call(arm_path_msg)){
 
-							}
-							else{
-								ROS_INFO("ERROR in arm_path service");
-								as_.setAborted();
-								return;
-							}
+			}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
 
 			ROS_INFO("closing right hand");
 			{boost::mutex::scoped_lock l(send_mutex);
@@ -233,12 +279,12 @@ public:
 
 			if(r_arm_path_cli_.call(arm_path_msg)){
 
-							}
-							else{
-								ROS_INFO("ERROR in arm_path service");
-								as_.setAborted();
-								return;
-							}
+			}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
 
 			break;
 
@@ -246,124 +292,124 @@ public:
 
 		case 'S':
 			/*open hand*/
-						ROS_INFO("opening right hand");
-						/*grasp_msg.strength = 0;
+			ROS_INFO("opening right hand");
+			/*grasp_msg.strength = 0;
 						pub_hand_grasp_right.publish(grasp_msg);
 						ros::Duration(0.3).sleep();
-						 */
+			 */
 
-						ros::Duration(0.3).sleep();
-						ROS_INFO("requesting transformation from perception");
-						//double *X,*Y,*Z,*R,*P,*YA;
-						X = new double(0);
-						Y = new double(0);
-						Z = new double(0);
-						R = new double(0);
-						P = new double(0);
-						YA = new double(0);
-						/*close hand*/
-						sandia_msg.name = "cylindrical";
-						{boost::mutex::scoped_lock l(send_mutex);
-						sandia_srv.request.grasp.name = "cylindrical";
-						sandia_srv.request.grasp.closed_amount = 0.4;
-						}
-						update_comand();
-						ros::spinOnce();
-						if (getObjectData(g,X,Y,Z,R,P,YA)){
-							//Grasp the other way if the grasp is over 90 degrees
-							if (!((-M_PI/2<*YA)&&(*YA<M_PI/2))) {
-								*YA=*YA+M_PI;
-							}
-							/*init vector for pipe*/
-							double sr=sin(*R),
-									cr=cos(*R),
-									sp=sin(*P),
-									cp=cos(*P),
-									sy=sin(*YA),
-									cy=cos(*YA);
-							Eigen::Matrix4f origin,transform,cross;
-							transform(0,0) =cp*cy; 	transform(0,1) = cy*sp*sr-cr*sy; 	transform(0,2) = sr*sy+cr*cy*sp; transform(0,3) = *X;
-							transform(1,0) = cp*sy; transform(1,1) = cr*cy+cy*sp*sr;	transform(1,2) = cr*sp*sy-cy*sr; transform(1,3) = *Y;
-							transform(2,0) = -sp; 	transform(2,1) = cp*sr; 			transform(2,2) = cp*cr; 		 transform(2,3) = *Z;
-							transform(3,0) = 0; 	transform(3,1) = 0; 				transform(3,2) = 0; 			 transform(3,3) = 1;
+			ros::Duration(0.3).sleep();
+			ROS_INFO("requesting transformation from perception");
+			//double *X,*Y,*Z,*R,*P,*YA;
+			X = new double(0);
+			Y = new double(0);
+			Z = new double(0);
+			R = new double(0);
+			P = new double(0);
+			YA = new double(0);
+			/*close hand*/
+			sandia_msg.name = "cylindrical";
+			{boost::mutex::scoped_lock l(send_mutex);
+			sandia_srv.request.grasp.name = "cylindrical";
+			sandia_srv.request.grasp.closed_amount = 0.4;
+			}
+			update_comand();
+			ros::spinOnce();
+			if (getObjectData(g,X,Y,Z,R,P,YA)){
+				//Grasp the other way if the grasp is over 90 degrees
+				if (!((-M_PI/2<*YA)&&(*YA<M_PI/2))) {
+					*YA=*YA+M_PI;
+				}
+				/*init vector for pipe*/
+				double sr=sin(*R),
+						cr=cos(*R),
+						sp=sin(*P),
+						cp=cos(*P),
+						sy=sin(*YA),
+						cy=cos(*YA);
+				Eigen::Matrix4f origin,transform,cross;
+				transform(0,0) =cp*cy; 	transform(0,1) = cy*sp*sr-cr*sy; 	transform(0,2) = sr*sy+cr*cy*sp; transform(0,3) = *X;
+				transform(1,0) = cp*sy; transform(1,1) = cr*cy+cy*sp*sr;	transform(1,2) = cr*sp*sy-cy*sr; transform(1,3) = *Y;
+				transform(2,0) = -sp; 	transform(2,1) = cp*sr; 			transform(2,2) = cp*cr; 		 transform(2,3) = *Z;
+				transform(3,0) = 0; 	transform(3,1) = 0; 				transform(3,2) = 0; 			 transform(3,3) = 1;
 
-									sr=sin(-3.13);
-									cr=cos(-3.13);
-									sp=sin(0.097);
-									cp=cos(0.097);
-									sy=sin(0.625);
-									cy=cos(0.625);
+				sr=sin(-3.13);
+				cr=cos(-3.13);
+				sp=sin(0.097);
+				cp=cos(0.097);
+				sy=sin(0.625);
+				cy=cos(0.625);
 
 
-							origin(0,0) =cp*cy; 	origin(0,1) = cy*sp*sr-cr*sy; 	origin(0,2) = sr*sy+cr*cy*sp; origin(0,3) = 0.502;
-							origin(1,0) = cp*sy; 	origin(1,1) = cr*cy+cy*sp*sr;	origin(1,2) = cr*sp*sy-cy*sr; origin(1,3) = -0.073;
-							origin(2,0) = -sp; 		origin(2,1) = cp*sr; 			origin(2,2) = cp*cr; 		  origin(2,3) = 0.32;
-							origin(3,0) = 0; 		origin(3,1) = 0; 				origin(3,2) = 0; 			  origin(3,3) = 1;
-							cross = transform*origin;
-							arm_path_msg.request.PositionDestination.x = cross(0,3);
-							arm_path_msg.request.PositionDestination.y = cross(1,3);
-							arm_path_msg.request.PositionDestination.z = cross(2,3);
-							arm_path_msg.request.AngleDestination.x = atan2((double)cross(2,1),(double)cross(2,2));
-							arm_path_msg.request.AngleDestination.y = atan2((double)-cross(2,0),sqrt(pow((double)cross(2,1),2) +pow((double)cross(2,2),2) ));
-							arm_path_msg.request.AngleDestination.z = atan2((double)cross(1,0),(double)cross(0,0));
-							arm_path_msg.request.time =4;
-							arm_path_msg.request.points =200;
-							ROS_INFO("recieved matrix from perception");
-							if (as_.isPreemptRequested() || !ros::ok())
-							{
-								ROS_INFO("ERROR not recieve matrix from perception");
-								ROS_ERROR("%s: Preempted", action_name_.c_str());
-								// set the action state to preempted
-								as_.setPreempted();
-								return;
-							}
-							/*moving hand to requested object*/
-							ROS_INFO("moving hand to requested object");
+				origin(0,0) =cp*cy; 	origin(0,1) = cy*sp*sr-cr*sy; 	origin(0,2) = sr*sy+cr*cy*sp; origin(0,3) = 0.502;
+				origin(1,0) = cp*sy; 	origin(1,1) = cr*cy+cy*sp*sr;	origin(1,2) = cr*sp*sy-cy*sr; origin(1,3) = -0.073;
+				origin(2,0) = -sp; 		origin(2,1) = cp*sr; 			origin(2,2) = cp*cr; 		  origin(2,3) = 0.32;
+				origin(3,0) = 0; 		origin(3,1) = 0; 				origin(3,2) = 0; 			  origin(3,3) = 1;
+				cross = transform*origin;
+				arm_path_msg.request.PositionDestination.x = cross(0,3);
+				arm_path_msg.request.PositionDestination.y = cross(1,3);
+				arm_path_msg.request.PositionDestination.z = cross(2,3);
+				arm_path_msg.request.AngleDestination.x = atan2((double)cross(2,1),(double)cross(2,2));
+				arm_path_msg.request.AngleDestination.y = atan2((double)-cross(2,0),sqrt(pow((double)cross(2,1),2) +pow((double)cross(2,2),2) ));
+				arm_path_msg.request.AngleDestination.z = atan2((double)cross(1,0),(double)cross(0,0));
+				arm_path_msg.request.time =4;
+				arm_path_msg.request.points =200;
+				ROS_INFO("recieved matrix from perception");
+				if (as_.isPreemptRequested() || !ros::ok())
+				{
+					ROS_INFO("ERROR not recieve matrix from perception");
+					ROS_ERROR("%s: Preempted", action_name_.c_str());
+					// set the action state to preempted
+					as_.setPreempted();
+					return;
+				}
+				/*moving hand to requested object*/
+				ROS_INFO("moving hand to requested object");
 
-							if(r_arm_path_cli_.call(arm_path_msg)){
+				if(r_arm_path_cli_.call(arm_path_msg)){
 
-							}
-							else{
-								ROS_INFO("ERROR in arm_path service");
-								as_.setAborted();
-								return;
-							}}
-						else {
-							ROS_INFO("ERROR in requesting transformation from perception");
-							as_.setAborted();
-							return;
-						}
+				}
+				else{
+					ROS_INFO("ERROR in arm_path service");
+					as_.setAborted();
+					return;
+				}}
+			else {
+				ROS_INFO("ERROR in requesting transformation from perception");
+				as_.setAborted();
+				return;
+			}
 
-						arm_path_msg.request.PositionDestination.z -= 0.2;
-						if(r_arm_path_cli_.call(arm_path_msg)){
+			arm_path_msg.request.PositionDestination.z -= 0.2;
+			if(r_arm_path_cli_.call(arm_path_msg)){
 
-										}
-										else{
-											ROS_INFO("ERROR in arm_path service");
-											as_.setAborted();
-											return;
-										}
+			}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
 
-						ROS_INFO("closing right hand");
-						{boost::mutex::scoped_lock l(send_mutex);
-						sandia_srv.request.grasp.name = "cylindrical";
-						sandia_srv.request.grasp.closed_amount = 1;
-						}
-						update_comand();
-						ros::spinOnce();
-						ros::Duration(1.2).sleep();
-						/*lifting*/
+			ROS_INFO("closing right hand");
+			{boost::mutex::scoped_lock l(send_mutex);
+			sandia_srv.request.grasp.name = "cylindrical";
+			sandia_srv.request.grasp.closed_amount = 1;
+			}
+			update_comand();
+			ros::spinOnce();
+			ros::Duration(1.2).sleep();
+			/*lifting*/
 
-						arm_path_msg.request.PositionDestination.z += 0.2;
+			arm_path_msg.request.PositionDestination.z += 0.2;
 
-						if(r_arm_path_cli_.call(arm_path_msg)){
+			if(r_arm_path_cli_.call(arm_path_msg)){
 
-										}
-										else{
-											ROS_INFO("ERROR in arm_path service");
-											as_.setAborted();
-											return;
-										}
+			}
+			else{
+				ROS_INFO("ERROR in arm_path service");
+				as_.setAborted();
+				return;
+			}
 
 			break;
 
