f851d52c86e44452239f9a23acb5ea32be76593f
Dave <dave.dovrat@gmail.com>
2013-06-14 17:13:48 +0300

    Rot_WalkingMode introduced
    Inserted also as Trans_WalkingMode until Trans_WalkingMode gets implemented
    
    NOT TESTED! I have no idea if AP_WalkingMode even implements the turn and translate functionality
    
    Signed-off-by: Dave <dave.dovrat@gmail.com>

---
+++ C42_DynamicLocomotion/src/Rot/Rot_WalkingMode.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+###################################################################################
+# File created by David Dovrat, 2013.
+# For Robil, under project Robil's license of choice
+# The code in this file is provided "as is" and comes with no warranty whatsoever
+###################################################################################
+
+import rospy
+from Abstractions.WalkingMode import *
+from AlinePose.AP_WalkingMode import *
+from AlinePose.AP_PathPlanner import *
+from DW.DW_WalkingMode import *
+from DW.DW_PathPlanner import *
+from C42_State.srv import *
+from C42_State.msg import StandingPosition
+
+class Rot_WalkingMode(WalkingMode):
+    def __init__(self,iTf):
+        self._LPPs = {'DW':DW_PathPlanner(),'AP':AP_PathPlanner()}
+        WalkingMode.__init__(self,self._LPPs['AP'])
+        self._WalkingModes = {'DW':DW_WalkingMode(iTf),'AP':AP_WalkingMode(iTf)}
+               
+        rospy.wait_for_service("/motion_state/info/standing_position")
+        self._srv_StandingPosition = rospy.ServiceProxy("/motion_state/info/standing_position", StandingPositionInfo)
+ 
+        self._SetRequiredWalkingMode()
+        
+        self._CurrentStandingMode = self._RequiredStandingMode
+ 
+    def Initialize(self,parameters):
+        WalkingMode.Initialize(self,parameters)        
+        
+        self._SetRequiredWalkingMode()
+        
+        if(self._CurrentStandingMode != self._RequiredStandingMode):
+            self._WalkingModes[self._RequiredStandingMode].SetPath(self._WalkingModes[self._CurrentStandingMode].GetPath())
+            self._LPP = self._LPPs[_RequiredStandingMode]
+        
+        self._WalkingModes[self._CurrentStandingMode].Initialize(parameters)
+        
+    def StartWalking(self):
+        self._WalkingModes[self._CurrentStandingMode].StartWalking()
+    
+    def Walk(self):
+        WalkingMode.Walk(self)
+        self._WalkingModes[self._CurrentStandingMode].Walk()
+    
+    def Stop(self):
+        WalkingMode.Stop(self)
+        self._WalkingModes[self._CurrentStandingMode].Stop()
+    
+    def EmergencyStop(self):
+        WalkingMode.Stop(self)
+        self._WalkingModes[self._CurrentStandingMode].EmergencyStop()
+    
+    def SetPath(self,Path):
+        self._WalkingModes[self._CurrentStandingMode].SetPath(Path)
+        self._LPP.SetPath(Path)
+        
+    def GetPath(self):
+        return self._WalkingModes[self._CurrentStandingMode].GetPath()
+        
+    def IsDone(self):
+        return self._WalkingModes[self._CurrentStandingMode].IsDone()
+
+    def IsReady(self):
+        return self._WalkingModes[self._CurrentStandingMode].IsReady()
+    
+    def Fitness(self,path):
+        return True
+
+################################################
+#                  "private"                   #
+################################################
+
+    def _SetRequiredWalkingMode(self):
+        standingPosition = self._srv_StandingPosition().info.state
+        if (StandingPosition.state_standing == standingPosition):
+            self._RequiredStandingMode = 'AP'
+        elif (StandingPosition.state_sitting == standingPosition):
+            self._RequiredStandingMode = 'DW'
--- C42_DynamicLocomotion/src/WalkingModeChooser.py
+++ C42_DynamicLocomotion/src/WalkingModeChooser.py
@@ -13,17 +13,20 @@ from DD.DD_WalkingMode import *
 from DW.DW_WalkingMode import *
 from CD.CD_WalkingMode import *
 from AlinePose.AP_WalkingMode import *
-from LocalPathPlanner import *
+#from Trans.Trans_WalkingMode import *
+from Rot.Rot_WalkingMode import *
 
 class WalkingModeChooserEnum:
-    DontCare,CD,QS,DD,DW,AP = range(6)
+    DontCare,CD,QS,DD,DW,AP,Rot,Trans = range(8)
 
 class WalkingModeChooser(WalkingModeChooserInterface):
 
     def __init__(self,prefferedMode):
         iTf = Interface_tf()
         
-        self._Modes = {'CD':CD_WalkingMode(),'QS':QS_WalkingMode(iTf),'DD':DD_WalkingMode(iTf),'DW':DW_WalkingMode(iTf),'AP':AP_WalkingMode(iTf)}
+        self._Modes = {'CD':CD_WalkingMode(),'QS':QS_WalkingMode(iTf),'DD':DD_WalkingMode(iTf),\
+                       'DW':DW_WalkingMode(iTf),'AP':AP_WalkingMode(iTf),'Rot':Rot_WalkingMode(iTf),\
+                       'Trans':Rot_WalkingMode(iTf)} #TODO Trans_WalkingMode
         self._Preferred = prefferedMode
         self._CurrentMode = prefferedMode
         self._Recommended = prefferedMode
@@ -31,7 +34,8 @@ class WalkingModeChooser(WalkingModeChooserInterface):
         self._bIsAppropriate = True
         self._EnumDictionary = {'CD':WalkingModeChooserEnum.CD,'QS':WalkingModeChooserEnum.QS,\
                                 'DD':WalkingModeChooserEnum.DD,'DW':WalkingModeChooserEnum.DW,\
-                                'AP':WalkingModeChooserEnum.AP}
+                                'AP':WalkingModeChooserEnum.AP,'Rot':WalkingModeChooserEnum.Rot,\
+                                'Trans':WalkingModeChooserEnum.Trans}
         
         self._debug_cmd_sub = rospy.Subscriber('walker_mode_override',Int32,self._walker_mode_handler)
         
